4b02c331bf9da694794266d4d71d3426c27cb3dd
Kai-Cheng Mei <kennymei47@gmail.com>
2024-12-01 17:29:40 -0800

    update5

--- main.cpp
+++ main.cpp
@@ -8,16 +8,20 @@ int main() {
 
     // load code
     ifstream file(codes.txt);
-    for (int i = 0; i << file.size; i++) {
+    string code;
+    if (!file) {
+        cout << "Error open file: " << endl;
+        return 0;
+    }
+    while (getline(file, code)) {
         tree.insert(code);
     }
     file.close();
     cout << "Finished loading into BST" << endl;
 
-
-
     // interactive menu
     int choice;
+    string a;
     do {
         cout << "Menu:" << endl;
         cout << "[1] add" << endl;
@@ -28,8 +32,19 @@ int main() {
 
         switch (choice) {
             case 1:
+                cout << "Enter code to add: ";
+                cin >> a;
+                tree.insertNode(a);
+                cout << "added code." << endl;
+                break;
             case 2:
+                cout << "Enter code to delete: ";
+                cin >> a;
+                tree.remove(a);
+                cout << "deleted code." << endl;
+                break;
             case 3:
+            
             case 4:
             case 5:
             default:
