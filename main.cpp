f87a5915582c500361cb4be97bef21fd5354f0e0
Kai-Cheng Mei <kennymei47@gmail.com>
2024-12-01 17:02:36 -0800

    update3

--- main.cpp
+++ main.cpp
@@ -4,7 +4,32 @@
 using namespace std;
 
 int main() {
+    IntBinaryTree tree;
+
+    // load code
+    ifstream file(codes.txt);
+
+
+
+
+    // interactive menu
+    int choice;
+    cout << "Menu:" << endl;
+    cout << "[1] add" << endl;
+    cout << "[2] delete" << endl;
+    cout << "[3] search" << endl;
+    cout << "" << endl;
+    cout << "Menu:" << endl;
+    switch (choice) {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
     
+    }
+
+
 
     return 0;
 }
