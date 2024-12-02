2d100ca5e4668094feb86a9d409d7ee2cc983123
Kai-Cheng Mei <kennymei47@gmail.com>
2024-12-01 17:10:18 -0800

    update4

--- main.cpp
+++ main.cpp
@@ -8,28 +8,35 @@ int main() {
 
     // load code
     ifstream file(codes.txt);
-
+    for (int i = 0; i << file.size; i++) {
+        tree.insert(code);
+    }
+    file.close();
+    cout << "Finished loading into BST" << endl;
 
 
 
     // interactive menu
     int choice;
-    cout << "Menu:" << endl;
-    cout << "[1] add" << endl;
-    cout << "[2] delete" << endl;
-    cout << "[3] search" << endl;
-    cout << "" << endl;
-    cout << "Menu:" << endl;
-    switch (choice) {
-        case 1:
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-    
-    }
+    do {
+        cout << "Menu:" << endl;
+        cout << "[1] add" << endl;
+        cout << "[2] delete" << endl;
+        cout << "[3] search" << endl;
+        cout << "[4] modify records" << endl;
+        cout << "[5] exit" << endl;
+
+        switch (choice) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            default:
+            cout << "Invalid, please enter 1~5." << endl;
+        }
 
 
-
+    } while(choice !=5)
     return 0;
 }
